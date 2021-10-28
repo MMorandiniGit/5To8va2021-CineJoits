14d3f077327d9ca2fa42f72ba5215297b720228c
MMorandiniGit <morandinimartin@gmail.com>
2021-10-28 11:33:31 -0300

    Update inserts

--- scripts/04 Inserts.sql
+++ scripts/04 Inserts.sql
@@ -1,21 +1,21 @@
 insert into cliente (nombre,apellido,mail,pass)
-            values  ("Ezequiel", "Kippes", "melacomodoblada@gmail.com", "kippes123"),
-                    ("Lucas","Medina","lucasselacome@gmail.com","lucas123"),
-                    ("Lucas","Nogales","lucasnogales@gmail.com","nogales123"),
-                    ("Josefina","Mendez","josemendez@gmail.com","jose123"),
-                    ("Marina","Hernandez","marihernandez@gmail.com","maru123"),
-                    ("Mariana","Gimenez","marugimenez@gmail.com","mariana123"),
-                    ("Pedro","Aries","pepoaries@gmail.com","pepo123");
+                values  ("Ezequiel", "Kippes", "melacomodoblada@gmail.com", "kippes123"),
+                        ("Lucas","Medina","lucasselacome@gmail.com","lucas123"),
+                        ("Lucas","Nogales","lucasnogales@gmail.com","nogales123"),
+                        ("Josefina","Mendez","josemendez@gmail.com","jose123"),
+                        ("Marina","Hernandez","marihernandez@gmail.com","maru123"),
+                        ("Mariana","Gimenez","marugimenez@gmail.com","mariana123"),
+                        ("Pedro","Aries","pepoaries@gmail.com","pepo123");
 
 insert into proyeccion (idSala,idPelicula,fechahora)
-            values  ();
+                values  ();
 
 insert into entrada (numeroentrada,idProyeccion,idCliente,)
-            values  ();
+                values  ();
 
 insert into pelicula (genero,fecha,nombre)
-            values   ();
+                values   ();
 
 
 insert into sala (idsala,piso,capacidad)
-            values ();
+                values ();
